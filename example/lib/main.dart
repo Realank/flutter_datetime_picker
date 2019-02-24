a8dbf842ff61d672817d5cbf8021a54ec13354ad
Kevin <kevin.gugelmann@outlook.com>
2019-02-24 17:20:28 +0000

    Uses month name (instead of number) for DatePicker

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -43,7 +43,7 @@ class HomePage extends StatelessWidget {
                     print('change $date');
                   }, onConfirm: (date) {
                     print('confirm $date');
-                  }, currentTime: DateTime.now(), locale: LocaleType.zh);
+                  }, currentTime: DateTime.now(), locale: LocaleType.en);
                 },
                 child: Text(
                   'show date picker(custom theme &date time range)',
