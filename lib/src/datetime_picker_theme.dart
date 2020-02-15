e7c81a00129b6c585966ed6b17421aad5544739b
aadarsh-patel <patelaadarsh0@gmail.com>
2020-02-16 01:25:10 +0530

    Added Custom font

--- lib/src/datetime_picker_theme.dart
+++ lib/src/datetime_picker_theme.dart
@@ -13,9 +13,9 @@ class DatePickerTheme extends Diagnosticable {
   final double itemHeight;
 
   const DatePickerTheme({
-    this.cancelStyle = const TextStyle(color: Colors.black54, fontSize: 16),
-    this.doneStyle = const TextStyle(color: Colors.blue, fontSize: 16),
-    this.itemStyle = const TextStyle(color: Color(0xFF000046), fontSize: 18),
+    this.cancelStyle = const TextStyle(color: Colors.black54, fontSize: 16, fontFamily: 'Varela'),
+    this.doneStyle = const TextStyle(color: Colors.blue, fontSize: 16, fontFamily: 'Varela'),
+    this.itemStyle = const TextStyle(color: Color(0xFF000046), fontSize: 18, fontFamily: 'Varela'),
     this.backgroundColor = Colors.white,
     this.headerColor,
     this.containerHeight = 210.0,
