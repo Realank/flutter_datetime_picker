0d230aa8ca7c5fe54b820fd1a8ad5be89b9f94a3
felixmo_trustMe <felixmo@moneysq.com>
2021-04-01 15:21:30 +0800

    Fixed the issue Android build

--- example/android/app/src/main/java/com/realank/example/MainActivity.java
+++ example/android/app/src/main/java/com/realank/example/MainActivity.java
@@ -3,12 +3,13 @@ package com.realank.example;
 import android.os.Bundle;
 
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        GeneratedPluginRegistrant.registerWith(this);
+        GeneratedPluginRegistrant.registerWith(new FlutterEngine(this));
     }
 }
