3593a09a7931c4130456a6a8b183b62f89b96d4a
Pedro Belfort <pedro.belfort@lutick.com>
2021-11-24 10:51:22 +0100

    Add borderRadius property to DatePickerTheme

--- lib/src/datetime_picker_theme.dart
+++ lib/src/datetime_picker_theme.dart
@@ -14,6 +14,8 @@ class DatePickerTheme with DiagnosticableTreeMixin {
   final double titleHeight;
   final double itemHeight;
 
+  final BorderRadius? borderRadius;
+
   const DatePickerTheme({
     this.cancelStyle = const TextStyle(color: Colors.black54, fontSize: 16),
     this.doneStyle = const TextStyle(color: Colors.blue, fontSize: 16),
@@ -23,5 +25,6 @@ class DatePickerTheme with DiagnosticableTreeMixin {
     this.containerHeight = 210.0,
     this.titleHeight = 44.0,
     this.itemHeight = 36.0,
+    this.borderRadius,
   });
 }
